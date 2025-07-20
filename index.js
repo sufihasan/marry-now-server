fa9da3c08793581cad19eae9c54a308555fe7415
sufihasan <hasan6nh@gmail.com>
2025-07-20 12:25:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bwnojnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const usersCollection = client.db('marryDB').collection('users');
+
+
+        //---------------
+        // make api start
+        //---------------
+
+
+        // -------- user related api-------
+
+        // save user info , if user already save just update last login
+        app.post('/users', async (req, res) => {
+            const email = req.body.email;
+            // console.log(req.headers);
+            const userExists = await usersCollection.findOne({ email });
+            if (userExists) {
+                // todo:update last login info
+
+                // Update last_log_in field
+                const result = await usersCollection.updateOne(
+                    { email },
+                    { $set: { last_log_in: new Date().toISOString() } }
+                );
+
+                return res.status(200).send({ message: 'user already exits', inserted: false, result });
+            }
+
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+
+        //---------------
+        // make api end
+        //---------------
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('marry now is ongoing')
+})
+
+app.listen(port, () => {
+    console.log(`marrynow is running on  ${port}`);
+})
